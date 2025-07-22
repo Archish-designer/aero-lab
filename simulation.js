f27100acef7e6f1f4b7cc07c993901f6d33e3453
Archish-designer <awa0002@cesc.vic.edu.au>
2025-07-22 22:36:03 +1000

    Update simulation.js

--- simulation.js
+++ simulation.js
@@ -10,7 +10,9 @@ let isRunning = false; // Boolean
 // The image for the car
 const carImage = new Image();
 carImage.src = "car.png";
-
+carImage.onload = () => {
+  console.log("Car image loaded!");
+};
 /**
  * FUNCTION: startSim() — is triggered by the Start button which Calculates the velocity and other external factors based on GUI inputs.
  */
