59a2c42c7477e6585a14bbc1426b4834dec7e245
Archish-designer <awa0002@cesc.vic.edu.au>
2025-07-25 12:03:15 +1000

    Update simulation.js

--- simulation.js
+++ simulation.js
@@ -47,22 +47,7 @@ function calculateNetVelocity() {
  * FUNCTION: animate()
  * This makes the cars image update based on its movement.
  */
-function animate()
-  /**
- * resetSim(): Resets the car to its starting position and stops the animation.
- */
-function resetSim() {
-  isRunning = false;        // Stop animation loop
-  position = 0;             // Reset car’s position to the far left
-  velocity = 0;             // resets the velocity
-  const canvas = document.getElementById("simCanvas");
-  const ctx = canvas.getContext("2d");
-  ctx.clearRect(0, 0, canvas.width, canvas.height);  
-  ctx.drawImage(carImage, position, 150, 250, 120);  // Draws the car at the starting position
-}
-
-
-{
+function animate(){
   if (isRunning) { // boolean check to see if the car is moving (selection control)
     const canvas = document.getElementById("simCanvas");
     const ctx = canvas.getContext("2d");
@@ -75,4 +60,16 @@ function resetSim() {
     position += velocity;
     requestAnimationFrame(animate);
   }
+}
+  /**
+ * resetSim(): Resets the car to its starting position and stops the animation.
+ */
+function resetSim() {
+  isRunning = false;        // Stop animation loop
+  position = 0;             // Reset car’s position to the far left
+  velocity = 0;             // resets the velocity
+  const canvas = document.getElementById("simCanvas");
+  const ctx = canvas.getContext("2d");
+  ctx.clearRect(0, 0, canvas.width, canvas.height);  
+  ctx.drawImage(carImage, position, 150, 250, 120);  // Draws the car at the starting position
 }
